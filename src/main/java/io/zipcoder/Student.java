a40cf3abc3439f6fdc7b4c9c766877a40d35e66d
Danny Cao <dannycao1997@yahoo.com>
2024-03-14 23:53:40 -0400

    started, added instance variables, constructor, and getters

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,30 @@
 package io.zipcoder;
+import java.util.ArrayList;
 
+
+
+//Student class
 public class Student {
+
+    // Define Instance Variables
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+
+    // Define Constructor
+    public Student(String firstName, String lastName, Double[] examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>();
+        for(Double score : examScores) {
+            this.examScores.add(score);
+        }
+
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
 }
