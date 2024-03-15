84794b92035a1bbf475e5469b5c75d7ff6d537f7
Danny Cao <dannycao1997@yahoo.com>
2024-03-15 00:03:07 -0400

    created methods for examscores / updating / work in progress

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 //Student class
 public class Student {
 
-    // Define Instance Variables
+    // Instance Variables
     String firstName;
     String lastName;
     ArrayList<Double> examScores;
 
 
-    // Define Constructor
+    // Constructor
     public Student(String firstName, String lastName, Double[] examScores){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -23,8 +23,41 @@ public class Student {
 
     }
 
+    // Getters and Setters
+
     public String getFirstName(){
         return firstName;
     }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(){
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getExamScores(){
+        return examScores;
+    }
+
+    public int getNumbersOfExamsTaken(){
+        return examScores.size();
+    }
+
+    public void addExamScore(double examScore){
+        examScores.add(examScore);
+    }
+
+    public void setExamScores(int examNumber, double newScore){
+        if(examNumber)
+    }
+
+
+
+
+
 
 }
